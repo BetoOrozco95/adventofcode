3078da4fde0bcb1dceecb340a9e8a80a2ecd9404
BetoOrozco95 <betoorozco95@hotmail.com>
2022-12-07 15:49:07 -0600

    add day 2 of 2022

---
+++ 2022/day2/day2.js
@@ -0,0 +1,48 @@
+import {readInput} from "../../utils/readInput.js";
+
+const input = readInput().split("\n");
+
+const win = {
+  A : 'C',
+  B : 'A',
+  C : 'B'
+}
+const lose = {
+  A : 'B',
+  B : 'C',
+  C : 'A'
+}
+const convertHand = {
+  X: 'A',
+  Y: 'B',
+  Z: 'C'
+}
+const defaultValues = {
+  A: 1,
+  B: 2,
+  C: 3,
+}
+let calculateFirstPart = (a,b) => {
+  if(!a || !b) return 0;
+  if( a === b) return defaultValues[b] + 3;
+  if(a === win[b]) return defaultValues[b] + 6;
+  if(a === lose[b]) return defaultValues[b];
+}
+
+let firstPartResult = input.reduce((pre, curr) => {
+  return  pre + calculateFirstPart(curr[0], convertHand[curr[2]]);
+}, 0);
+
+let calculateSecondPart = (a,b) => {
+  if(!a || !b) return 0;
+  if(b === 'B') return defaultValues[a] + 3;
+  if(b === 'A') return defaultValues[win[a]];
+  if(b === 'C') return defaultValues[lose[a]] + 6;
+}
+
+let secondPartResult = input.reduce((pre, curr) => {
+  return  pre + calculateSecondPart(curr[0], convertHand[curr[2]]);
+}, 0);
+
+console.log('Part 1: ' + firstPartResult);
+console.log('Part 2: ' + secondPartResult);
