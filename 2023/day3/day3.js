4d979076fa64a5e69df77f26c9bbe8b9502944aa
BetoOrozco95 <betoorozco95@hotmail.com>
2023-12-04 22:06:24 -0600

    day 3 part 2

--- 2023/day3/day3.js
+++ 2023/day3/day3.js
@@ -2,47 +2,66 @@ import {readInput} from "../../utils/index.js";
 
 const input = readInput().split("\n");
 
-const hasAdjacent = (i, j) => {
+const getNumber = (str, idx) => {
+  let number = str[idx];
+  if(!/[0-9]/.test(number)) return NaN;
+  let leftIdx = 1;
+  let rightIdx = 1;
+  while(/[0-9]/.test(str[idx - leftIdx])) {
+    number = `${str[idx - leftIdx]}${number}`;
+    leftIdx++;
+  }
+  while(/[0-9]/.test(str[idx + rightIdx])) {
+    number = `${number}${str[idx + rightIdx]}`;
+    rightIdx++;
+  }
+  return +number;
+}
+
+const hasAdjacentNumber = (i, j, part1 = false) => {
   const line = input[i];
   const upLine = input[i - 1];
   const downLine = input[i + 1];
   const adjacent = [
-    line?.[j + 1],
-    line?.[j - 1],
-    upLine?.[j],
-    upLine?.[j - 1],
-    upLine?.[j + 1],
-    downLine?.[j],
-    downLine?.[j - 1],
-    downLine?.[j + 1],
-  ].filter(Boolean);
-  return  adjacent.some(c => !/[0-9.]/.test(c) );
-}
+    [line, j + 1],
+    [line, j - 1],
+    [upLine, j],
+    [upLine, j - 1],
+    [upLine, j + 1],
+    [downLine, j],
+    [downLine, j - 1],
+    [downLine, j + 1],
+  ].map(([str, idx]) => getNumber(str, idx)).filter(Boolean);
 
-let validPartNumber = [];
-let partNumber = [];
-
-for (let i = 0; i < input.length; i++) {
-  const line = input[i];
-  for (let j = 0; j < line.length; j++) {
-    const c = line[j];
-    if (Number(c) >= 0) {
-      partNumber.push({ i: j, n: c})
-      if(line.length - 1 !== j){
-        continue;
-      }
-    }
-    if(partNumber.length > 0) {
-      const number = partNumber.reduce((str, part) => str + part.n, '');
-      const index = partNumber.map((part) => part.i);
-      if(index.some(idx => hasAdjacent(i, idx))) {
-        validPartNumber.push(+number);
-      }
-    }
-    partNumber = [];
+  const removeDuplicates = [...new Set(adjacent)];
+  if(part1) {
+    return removeDuplicates.reduce((sum, number) => sum + number, 0);
   }
+  if(removeDuplicates.length === 2) {
+    return removeDuplicates[0] * removeDuplicates[1];
+  }
+  return  0;
 }
 
-console.log(validPartNumber)
-console.log(validPartNumber.reduce((sum, num) => sum + num, 0))
+const resolve = () => {
+  let resultPart1 = 0;
+  let resultPart2 = 0;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      if(!/[0-9.]/.test(input[i][j])) {
+        resultPart1 += hasAdjacentNumber(i, j, true);
+      }
+      if (input[i][j] === '*') {
+        resultPart2 += hasAdjacentNumber(i, j);
+      }
+    }
+  }
+  return {
+    resultPart1,
+    resultPart2
+  };
+}
+const {resultPart1, resultPart2} = resolve();
 
+console.log(`Result part one: ${resultPart1}`);
+console.log(`Result part one: ${resultPart2}`);
